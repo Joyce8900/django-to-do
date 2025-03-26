30e0600e9e76c1729826d3b70ce8d00b06f6955c
Joyce8900 <joycesantos8900@gmail.com>
2025-03-26 16:42:30 -0300

    add:redirecionamento de cadastro para listar

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'todo'
+# app_name = 'todo'
 
 urlpatterns = [
-  path('todo/cadastrar_tarefas', views.cadastrar_tarefas, name='cadastrar_tarefas'),
-]
+  path('todo/cadastrar_tarefas/', views.cadastrar_tarefas, name='cadastrar_tarefas'),
+  path('todo/listar_tarefas/', views.listar_tarefas, name='listar_tarefas'),
+
+] 
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
+
+def listar_tarefas(request):
+  todos = Todo.objects.all()
+  return render(request, 'listar_tarefas.html', {'todos': todos})
 
 # Create your views here.
 def cadastrar_tarefas(request):
@@ -9,4 +13,8 @@ def cadastrar_tarefas(request):
     description = request.POST.get('description')
     todo =Todo(title=title, description=description)
     todo.save()
+    return redirect('listar_tarefas')
   return render(request, 'cadastrar_tarefas.html')
+  
+
+
